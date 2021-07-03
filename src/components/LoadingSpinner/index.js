3c399f51ea856190cdcabd1476bc1612f8e76b46
Kaustubh <kaustubh.kesarkar@gmail.com>
2021-07-04 00:42:05 +0530

    added color value

--- src/components/LoadingSpinner/index.js
+++ src/components/LoadingSpinner/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {ActivityIndicator} from 'react-native';
 
 const LoadingSpinner = props => (
-  <ActivityIndicator size="large" color="#98CC83" />
+  <ActivityIndicator size="large" color="#00BFFF" />
 );
 
 export default LoadingSpinner;
